0331d0423430632ba4a3b339402a7729aa4a7dd5
PhucNH <phucnh.dev@gmail.com>
2021-03-23 17:01:59 +0700

    Update factory address code and init code hash

--- src/constants.ts
+++ src/constants.ts
@@ -19,9 +19,9 @@ export enum Rounding {
   ROUND_UP
 }
 
-export const FACTORY_ADDRESS = '0xBCfCcbde45cE874adCB698cC183deBcF17952812'
+export const FACTORY_ADDRESS = '0x01bF7C66c6BD861915CdaaE475042d3c4BaE16A7'
 
-export const INIT_CODE_HASH = '0xd0d4c4cd0848c93cb4fd1f498d7013ee6bfb25783ea21593d5834f5d250ece66'
+export const INIT_CODE_HASH = '0xe2e87433120e32c4738a7d8f3271f3d872cbe16241d67537139158d90bac61d3'
 
 export const MINIMUM_LIQUIDITY = JSBI.BigInt(1000)
 
@@ -34,6 +34,7 @@ export const FIVE = JSBI.BigInt(5)
 export const TEN = JSBI.BigInt(10)
 export const _100 = JSBI.BigInt(100)
 export const _998 = JSBI.BigInt(998)
+export const _997 = JSBI.BigInt(997)
 export const _1000 = JSBI.BigInt(1000)
 
 export enum SolidityType {
